1af195d65350f35c46278e5da0e8c485cec9ed6e
AndiLinUnab <alin@unab.edu.co>
2026-02-14 17:49:51 -0500

    Vista metas creada

---
+++ resources/views/metas.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Metas</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <nav>
+        <a href="/perfil">Perfil</a>
+        <a href="/perfil/intereses">Intereses</a>
+        <a href="/perfil/habilidades">Habilidades</a>
+        <a href="/perfil/metas">Metas</a>
+    </nav>
+
+    <h1>Mis Metas Profesionales</h1>
+    <p>Convertirme en desarrollador backend profesional y arquitecto de software.</p>
+</body>
+</html>
