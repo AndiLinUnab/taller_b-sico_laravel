22b917791f980a45ba821e5ce5ec9e40d1bc76ac
AndiLinUnab <alin@unab.edu.co>
2026-02-14 17:44:36 -0500

    Vista perfil creada

---
+++ resources/views/perfil.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mi Perfil</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <nav>
+        <a href="/perfil">Perfil</a>
+        <a href="/perfil/intereses">Intereses</a>
+        <a href="/perfil/habilidades">Habilidades</a>
+        <a href="/perfil/metas">Metas</a>
+    </nav>
+
+    <h1>Mi Perfil</h1>
+    <p>Estudiante de Ingeniería de Sistemas en la UNAB.</p>
+</body>
+</html>
