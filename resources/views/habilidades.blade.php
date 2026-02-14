26725ba6e94e79fcc554938a80b25ca69f296da7
AndiLinUnab <alin@unab.edu.co>
2026-02-14 17:48:01 -0500

    Vista habilidades creada

---
+++ resources/views/habilidades.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Habilidades</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <nav>
+        <a href="/perfil">Perfil</a>
+        <a href="/perfil/intereses">Intereses</a>
+        <a href="/perfil/habilidades">Habilidades</a>
+        <a href="/perfil/metas">Metas</a>
+    </nav>
+
+    <h1>Mis Habilidades</h1>
+    <ul>
+        <li>PHP</li>
+        <li>Laravel</li>
+        <li>Git</li>
+    </ul>
+</body>
+</html>
