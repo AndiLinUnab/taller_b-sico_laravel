cfaa8861d55cc44c837cbdd0626ec77fceb0c374
AndiLinUnab <alin@unab.edu.co>
2026-02-14 17:46:03 -0500

    Vista intereses creada

---
+++ resources/views/intereses.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Intereses</title>
+    <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+</head>
+<body>
+    <nav>
+        <a href="/perfil">Perfil</a>
+        <a href="/perfil/intereses">Intereses</a>
+        <a href="/perfil/habilidades">Habilidades</a>
+        <a href="/perfil/metas">Metas</a>
+    </nav>
+
+    <h1>Mis Intereses</h1>
+    <ul>
+        <li>Desarrollo Backend</li>
+        <li>Inteligencia Artificial</li>
+        <li>Diseño de Software</li>
+    </ul>
+</body>
+</html>
